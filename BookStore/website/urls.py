77b665d542cce7fe30a26cda131e01ab18f72cb5
Deft Nguyen <jimmi2051@gmail.com>
2018-12-27 11:15:02 +0700

    Update View Website

--- BookStore/website/urls.py
+++ BookStore/website/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
-    path('category/<int:id>', views.index, name='category'),
-    path('detail/<int:id>', views.detail, name='detail'),
+    #path('category/<int:id>', views.index, name='category'),
+    #path('detail/<int:id>', views.detail, name='detail'),
     path('search', views.search, name='search'),
     path('cart', views.cart, name='cart'),
     path('checkout', views.checkout, name='checkout'),
@@ -16,4 +16,6 @@ urlpatterns = [
     # path('', views.index, name='index'),
     # path('', views.index, name='index'),
     # path('', views.index, name='index'),
+    path('category', views.category, name='category'),
+    path('detail', views.detail, name='detail'),
 ]
--- BookStore/website/views.py
+++ BookStore/website/views.py
@@ -6,19 +6,27 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'website/page/index.html', {})
 def login(request):
-    return render(request, 'website/page/index.html', {})
+    return render(request, 'website/page/login.html', {})
 def logout(request):
     return render(request, 'website/page/index.html', {})   
-def category(request, id):
-    return render(request, 'website/page/index.html', {})
-def detail(request, id):
-    return render(request, 'website/page/index.html', {})
+# def category(request, id):
+#     return render(request, 'website/page/category.html', {})
+
+def category(request):
+    return render(request, 'website/page/category.html', {})
+
+# def detail(request, id):
+#     return render(request, 'website/page/index.html', {})
+
+def detail(request):
+    return render(request, 'website/page/detail.html', {})
+
 def search(request):
-    return render(request, 'website/page/index.html', {})
+    return render(request, 'website/page/search.html', {})
 def cart(request):
-    return render(request, 'website/page/index.html', {})
+    return render(request, 'website/page/cart.html', {})
 def checkout(request):
-    return render(request, 'website/page/index.html', {})
+    return render(request, 'website/page/checkout.html', {})
 def profile(request):
     return render(request, 'website/page/index.html', {})
 def order(request):
